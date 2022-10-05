ae0023256b62c166bec1f394ad0b2f3b42280820
Abigail Cox <abigailgcox@outlook.com>
2022-10-05 20:19:13 +0100

    Adds user input functionality and input verification

--- milestone_1.py
+++ milestone_1.py
@@ -5,3 +5,10 @@ print(word_list)
 
 word = random.choice(word_list)
 print(word)
+
+guess = input("Enter a single letter of the alphabet:\t")
+
+if len(guess) == 1 and guess.isalpha() == True:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input. The input needs to be a single letter of the alphabet.")
